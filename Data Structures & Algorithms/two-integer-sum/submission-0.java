a1754c25e2189c8f2c5203b1c6145d3c0f265e18
Hemnaath <hemnaath.usa@gmail.com>
2026-06-06 18:47:39 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> output = new LinkedList<Integer>();
+
+        for(int i=0;i<=nums.length;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]+nums[j]==target){
+                    output.add(i);
+                    output.add(j);
+                }
+            }
+        }
+    return output.stream().mapToInt(Integer::intValue).toArray();
+
+        
+    }
+}
